0d921f89a434fca4ffadf880efa84afd99785747
Chanda <56906759+Chanda-Abdul@users.noreply.github.com>
2022-11-07 03:11:39 -0800

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  IP_GEOLOCATION_API_KEY: '6c360f10ae8d42b4af991b1cae680046',
-  GOOGLE_MAPS_API_KEY: 'AIzaSyB-y5UNC3MSnc6m7PTRSjFy4KnXNOq8GRI'
-};
